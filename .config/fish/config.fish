64e9e2198aeb4189f14cc33f0a9b1945ebe65552
Jean Casademont <jean.casademont@gmail.com>
2019-08-20 16:24:55 +0200

    Abbr for git and vim

---
+++ .config/fish/config.fish
@@ -0,0 +1,21 @@
+
+set -gx PATH $HOME/opt/bin $HOME/opt/kotlinc/bin $PATH
+
+# ABBREVIATIONS
+
+## git
+abbr g='git'
+abbr gst='git status'
+abbr gd='git diff'
+abbr gb='git branch'
+abbr gl='git pull'
+abbr gp='git push'
+abbr ga='git add -u'
+abbr gc='git commit'
+abbr gc!='git commit --amend'
+abbr gm='git checkout master'
+abbr gcb='git checkout -b'
+abbr gct='git checkout'
+
+## vim
+abbr vim='nvim'
